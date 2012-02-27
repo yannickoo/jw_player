2113a483b2618d8470a2fa10dfa9a9218c670860
Rick Vugteveen <rickvug@gmail.com>
2012-02-27 11:34:25 +0000

    Issue #1395584: Follow up - Allow  to be passed to the theme function.

--- theme/jw_player.tpl.php
+++ theme/jw_player.tpl.php
@@ -9,13 +9,13 @@
  * - $height: Height of the video player.
  * - $sources: An array of files to be played.
  * - $jw_player_inline_js_code: JSON data with configuration settings for the video player.
- * - $poster: URL to an image to be used for the poster (ie. preview image) for this video.
+ * - $image: URL to an image to be used for the poster (ie. preview image) for this video.
  *
  * @see template_preprocess_jw_player()
  */
 ?>
 <div class="jwplayer-video">
-  <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls" preload="none"<?php if(isset($poster)) : ?> poster="<?php print $poster ?>"<?php endif ?>>
+  <video id="<?php print $html_id ?>" width="<?php print $width ?>" height="<?php print $height ?>" controls="controls" preload="none"<?php if(isset($image)) : ?> poster="<?php print $image ?>"<?php endif ?>>
   <?php foreach ($sources as $source) { ?>
     <source src="<?php print $source['file_path']; ?>"<?php if (isset($source['file_mime'])): ?> type="<?php print $source['file_mime'] ?>"<?php endif ?> />
   <?php };?>
